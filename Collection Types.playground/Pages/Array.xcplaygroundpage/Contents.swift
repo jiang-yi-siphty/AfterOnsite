cd0c0c117a2b89c3710376ac250225a99b96b58f
Yi JIANG <jiang.yi@siphty.com>
2018-09-24 22:50:19 +1000

    Update: Practise assembled and split String by Array.

--- Collection Types.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -4,6 +4,22 @@ import Foundation
 
 var str = "Hello, playground"
 
-var stringArray = ["a", nil, "b"]
+var stringArray = ["aString", "bString", "cString", nil, "eString"]
 var result = stringArray.compactMap { $0 }.joined(separator: "\n")
 print(result)
+
+// 合并字符串数组
+let arr1: [String] = ["1", "", "1", "2"]
+arr1.joined(separator: "|")  // 1||1|2
+
+// 合并整型数组
+let arr2: [Int] = [1, 2, 3, 4]
+arr2.map(String.init).joined(separator: "|")
+
+// 拆分字符串
+let str1: String = "1,2,,,,3"
+str1.split(separator: ",")
+str1.components(separatedBy: ",")
+
+
+
